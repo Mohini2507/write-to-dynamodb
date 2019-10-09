4ae57ec7bdbffdd345571a7491f6cf12a4204e3b
Mohini2507 <44153347+Mohini2507@users.noreply.github.com>
2019-10-09 15:23:48 +0530

    test

--- dynamo.tf
+++ dynamo.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table_item" "example" {
   table_name = "restscking-data-store"
   hash_key   = "CodeBuildId"
-  
+  codeBuildIdValue = $(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
   item = <<ITEM
 {
-  "CodeBuildId": {"S": "$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')"},
+  "CodeBuildId": {"S": "codeBuildIdValue"},
   "StageName": {"S": "xyz"},
   "Status": {"S": "XYZ"},
   "TimeStamp": {"S": "XYZ"}
