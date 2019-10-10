2181c198121c1605cf2f38834ae9be3e1fb0249d
Mohini2507 <44153347+Mohini2507@users.noreply.github.com>
2019-10-10 13:03:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export codeBuildId=$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
+echo "Code build id is " $codeBuildId
 aws dynamodb put-item --table-name restscking-data-store --item "{""CodeBuildId"":{""S"",""12345""},""StageName"":{""S"",""abc""},""Status"":{""S"",""abc""},""TimeStamp"":{""S"",""abc""}}" --return-consumed-capacity TOTAL
