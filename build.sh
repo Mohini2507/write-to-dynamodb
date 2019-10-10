5e176c460246af6a02d2a6f0ceffe995dd8d781d
Mohini2507 <44153347+Mohini2507@users.noreply.github.com>
2019-10-10 13:53:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 export codeBuildId=$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
+export codeBuildStartTime=$CODEBUILD_START_TIME
+
 echo "Code build id is " $codeBuildId
+echo "Code build start time is " $CODEBUILD_START_TIME
 #aws dynamodb put-item --table-name restscking-data-store --item "{""CodeBuildId"":{""S"",""12345""},""StageName"":{""S"",""abc""},""Status"":{""S"",""abc""},""TimeStamp"":{""S"",""abc""}}" --return-consumed-capacity TOTAL
 aws dynamodb put-item \
-    --table-name restscking-data-store \
+    --table-name restacking-data-store \
     --item '{
-        "CodeBuildId": {"S": "No One You Know"},
-        "StageName": {"S": "$codeBuildId"} ,
-        "Status": {"S": "'$codeBuildId'"} ,
-        "TimeStamp": {"S": "Somewhat Famous"} }' \
+        "CodeBuildId": {"S": "'$codeBuildId'"},
+        "StageName": {"S": "Fetching JSON values from S3"} ,
+        "Status": {"S": "In-progress"} ,
+        "StartTime": {"S": "'$codeBuildStartTime'"} ,
+        "EndTime": {"S": "'$codeBuildStartTime'"} }' \
     --return-consumed-capacity TOTAL
