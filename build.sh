5e81ca2660048230bd0d4985bce29b475734ee74
Mohini2507 <44153347+Mohini2507@users.noreply.github.com>
2019-10-10 13:10:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 
 export codeBuildId=$(echo $CODEBUILD_BUILD_ID | awk -F":" '{print $2}')
 echo "Code build id is " $codeBuildId
-aws dynamodb put-item --table-name restscking-data-store --item "{""CodeBuildId"":{""S"",""12345""},""StageName"":{""S"",""abc""},""Status"":{""S"",""abc""},""TimeStamp"":{""S"",""abc""}}" --return-consumed-capacity TOTAL
+#aws dynamodb put-item --table-name restscking-data-store --item "{""CodeBuildId"":{""S"",""12345""},""StageName"":{""S"",""abc""},""Status"":{""S"",""abc""},""TimeStamp"":{""S"",""abc""}}" --return-consumed-capacity TOTAL
+aws dynamodb put-item \
+    --table-name restscking-data-store \
+    --item '{
+        "CodeBuildId": {"S": "No One You Know"},
+        "StageName": {"S": "Call Me Today"} ,
+        "Status": {"S": "Call Me Today"} ,
+        "TimeStamp": {"S": "Somewhat Famous"} }' \
+    --return-consumed-capacity TOTAL
