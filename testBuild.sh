11219cffad70747ac487296d734376f9fbff1bc2
Mohini2507 <44153347+Mohini2507@users.noreply.github.com>
2019-10-14 15:39:41 +0530

    Update testBuild.sh

--- testBuild.sh
+++ testBuild.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-###################################################
-# Stage 1: Fetching JSON values from S3 start time#
-###################################################
 export codeBuildStartTime=`date +%s%N | cut -b1-13`
 ./writeInputToDBStartTime.sh $codeBuildStartTime
 
-#################################################
-# Stage 1: Fetching JSON values from S3 end time#
-#################################################
 export codeBuildEndTime=`date +%s%N | cut -b1-13`
 ./writeInputToDBEndTime.sh $codeBuildEndTime
