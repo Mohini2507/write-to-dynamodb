ca548b85adc0b5189cb93be0fd5ed239e42a3def
Mohini2507 <44153347+Mohini2507@users.noreply.github.com>
2019-10-14 15:53:18 +0530

    Update testBuild.sh

--- testBuild.sh
+++ testBuild.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 export codeBuildStartTime=`date +%s%N | cut -b1-13`
-chmod +x writeInputToDBStartTime.sh
 ./writeInputToDBStartTime.sh $codeBuildStartTime
 
 export codeBuildEndTime=`date +%s%N | cut -b1-13`
-chmod +x writeInputToDBEndTime.sh
 ./writeInputToDBEndTime.sh $codeBuildEndTime
